53ab35eba20e0d929f2c0afeae76c3890a39006a
Рената Цікалова <ts.renata98@gmail.com>
2021-12-04 15:50:32 +0200

    Changes

--- Itea HW2/Program.cs
+++ Itea HW2/Program.cs
@@ -77,7 +77,7 @@ namespace HW2
 
             Console.WriteLine("\n" + Task4(1, 6, 9));
 
-            Console.WriteLine("\n" + Task5(67));
+            Console.WriteLine("\n" + Task5(67));///
         }
     }
 }
