841723351a256f7363e91649c81a75080e95f319
Рената Цікалова <ts.renata98@gmail.com>
2021-12-06 13:29:47 +0200

    Changes 1-6

--- Itea HW2/Program.cs
+++ Itea HW2/Program.cs
@@ -6,78 +6,129 @@ namespace HW2
     {
         static double Task1(int a, int b)
         {
+            double result;
+
             if (a > b)
-                return a + b;
+            {
+                result = a + b;
+            }
             else if (a == b)
-                return a * b;
-            else
-                return a - b;
+            {
+                result = a * b;
+            }
+            else 
+            {
+                result = a - b;
+            }
+
+            return result;
         }
 
         static string Task2(int x, int y)
         {
+            string n="lies on axis";
+
             if (x > 0 && y > 0)
-                return "Quarte 1";
+            {
+                n = "Quarte 1";
+            }
             else if (x < 0 && y > 0)
-                return "Quarte 2";
+            {
+                n = "Quarte 2";
+            }
             else if (x < 0 && y < 0)
-                return "Quarte 3";
+            {
+                n = "Quarte 2";
+            }
             else if (x > 0 && y < 0)
-                return "Quarte 4";
-            else
-                return "lies on the axis";
+            {
+                n = "Quarte 4";
+            }
+
+            return n;
         }
 
-        static (int, int, int) Task3(int a, int b, int c)
+        static void Task3(ref int a, ref int b, ref int c)
         {
-            if (a > b && a > c)
-                return (a, Math.Max(b, c), Math.Min(b, c));
-            else if (b > a && b > c)
-                return (b, Math.Max(a, c), Math.Min(a, c));
-            else
-                return (c, Math.Max(a, b), Math.Min(a, b));
+            if (a < b)
+            {
+                (a, b) = (b, a);
+            }
+            if (a < c)
+            {
+                (a, c) = (c, a);
+            }
+            if (b < c)
+            {
+                (b, c) = (c, b);
+            }
         }
 
-        static string Task4(double a, double b, double c)
+        static (double,double) Task4(double a, double b, double c)
         {
-            var d = b * b - 4 * a * c;
-            var result = (Math.Round((-b + Math.Sqrt(d)) / 2 * a, 3), Math.Round((-b - Math.Sqrt(d)) / 2 * a, 3));
+            double X1;
+            double X2;
 
-            if (d > 0)
-                return $"x1={result.Item1} x2={result.Item2}";
-            else if (d == 0)
-                return $"x={result.Item1}";
+            double d = b * b - 4 * a * c;
+
+            if (d>=0)
+            {
+                X1 = Math.Round((-b + Math.Sqrt(d)) / 2 * a, 3);
+                X2 = Math.Round((-b - Math.Sqrt(d)) / 2 * a, 3);
+            }
             else
-                return "d<0";
+            {
+                X1 = 0;
+                X2 = 0;
+            }
+
+            return (X1, X2);
         }
 
         static string Task5(int number)
         {
+            string result;
+
             string[] Array1 = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
             string[] Array2 = { "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
             string[] Array3 = { "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 
             int b = number % 10;
-            int a = (number - b) / 10;
+            int a = number/ 10;
 
-            if (number > 10 && number < 20) return Array2[number - 11];
-            if (b == 0) return Array3[a - 1];
+            if (number > 10 && number < 20)
+            {
+                result=Array2[number - 11];
+            }
+            else if (b == 0)
+            {
+                result=Array3[a - 1];
+            }
+            else
+            {
+                result = Array3[a - 1] + ' ' + Array1[b - 1];
+            }
 
-            return Array3[a - 1] + ' ' + Array1[b - 1];
+            return result;
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine(Task1(-2, 2));
 
-            Console.WriteLine("\n" + Task2(1, -2));
+            Console.WriteLine("\n" + Task2(0, -2));
 
-            (int, int, int) result3 = Task3(-54, 7897, 3);
-            Console.WriteLine("\n" + result3);
+            int a = -1;
+            int b = 43;
+            int c = 100;
 
-            Console.WriteLine("\n" + Task4(1, 6, 9));
+            Task3(ref a, ref b, ref c);
+            Console.WriteLine("\n" + $"{a},{b},{c}");
 
-            Console.WriteLine("\n" + Task5(67));
+            (double, double) result = Task4(5, 3, 7);
+            Console.WriteLine("\n" + result);
+  
+            Console.WriteLine("\n" + Task5(11));
         }
     }
 }
